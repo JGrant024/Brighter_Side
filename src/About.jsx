c02b5820629a2be5b1956ce56cf83799cbe6a689
JGrant024 <jonathan.grant23@icloud.com>
2024-04-19 13:20:48 -0400

    updated about page, created a file for images to be stored locally

--- src/About.jsx
+++ src/About.jsx
@@ -2,6 +2,10 @@ function about() {
   return (
     <>
       <div> About Us!</div>
+      <p>
+        Welcome! Below you will find some of our Brighter Side Packages! Find
+        the one that best suites your needs.{" "}
+      </p>
     </>
   );
 }
